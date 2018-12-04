8f7037420843946fa72a3a8ce8b992305bafc470
Austin Wynn <GitUpdateWynn@yahoo.com>
2018-12-04 14:38:39 -0500

    First Commit: Initializing folder contents

---
+++ Hello_World.py
@@ -0,0 +1,12 @@
+#Simple Hello World in Python
+print("Hello World");print("Line 2")
+print(3+2)
+
+
+
+
+
+
+
+
+
---
+++ Numbers.py
@@ -0,0 +1,21 @@
+#Simple Math
+import math
+
+g = math.pi*5
+i = math.e*3
+float(i)
+float(g)
+print(i,"\n",g)
+
+x=10.67-2.345
+print(math.floor(x))
+
+y = 10-25
+print(math.fabs(y))
+
+r =13.352362
+print(math.modf(r),round(r,4))
+
+
+print(pow(2,3))
+
